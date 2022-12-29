e7a802397c3ee4b08b3e6176e075713ead0d1d57
Jeremy Warren <jeddynwarren@gmail.com>
2022-12-28 22:16:53 -0700

    Initial commit

---
+++ Desktop/CoreBlue2th/CoreBlue2th/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  CoreBlue2th
+//
+//  Created by Jeremy Warren on 12/28/22.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct ContentView: View {
+    @ObservedObject private var contentVM = ContentViewModel()
+    @State private var audioPlayer: AVAudioPlayer!
+    
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+            List(contentVM.peripheralNames, id: \.self) { peripheral in
+                Text(peripheral)
+            }
+                Button {
+                    self.audioPlayer.play()
+                } label: {
+                    Text("Mambo #5")
+                }
+            }
+            .onAppear {
+                
+                let audioPath = Bundle.main.path(forResource: "Mambo6", ofType: "mp3")!
+                let url = URL(string: audioPath)!
+                do {
+                    
+                    try self.audioPlayer = AVAudioPlayer(contentsOf: url)
+                } catch {
+                    print(error.localizedDescription, error)
+                }
+            }
+        }
+        .navigationTitle("Bluetooth")
+    }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desktop/CoreBlue2th/CoreBlue2th/ContentViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ContentViewModel.swift
+//  CoreBlue2th
+//
+//  Created by Jeremy Warren on 12/28/22.
+//
+
+import Foundation
+import CoreBluetooth
+
+class ContentViewModel: NSObject, ObservableObject {
+  
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral] = []
+    @Published var peripheralNames: [String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+        
+    }
+    
+}
+
+extension ContentViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            self.peripherals.append(peripheral)
+            self.peripheralNames.append(peripheral.name ?? "unnamed device")
+        }
+    }
+}
